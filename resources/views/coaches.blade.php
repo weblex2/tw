af923d256d22a1ecd62618dfa329a439af5a2a96
Thomas <thomaswittke@web.de>
2023-10-06 17:09:58 +0200

    Foto Coach Emma

--- resources/views/coaches.blade.php
+++ resources/views/coaches.blade.php
@@ -121,7 +121,7 @@
         </div>
 
         <!-- 6. row -->
-        <div class="flex h-96 w-full bg-red-500 bg-center bg-cover bg-[url('../../public/img/Foto-Coach-Emma-III.jpg')]">
+        <div class="flex h-96 w-full bg-red-500 bg-center bg-cover bg-[url('../../public/img/Foto-Coach-Emma-bearbeitet-180x180.jpg')]">
         </div>
         <div class="coach-info-1">
             <div>
