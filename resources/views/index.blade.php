9b1d39300a9d8583be035dae9da7c4e0a4adec7a
Thomas <thomaswittke@web.de>
2024-02-17 12:52:57 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,7 @@
                 <p><strong>Team:<br></strong> Gators United,<br> Juniors Allgirl Level 3</p><br>
                 <strong>1. Platz<br></strong> mit 7,99 Punkten<br>
                 </div></strong> </a><br><br>
-                <class="flex h-96 w-full bg-red-500 bg-center bg-cover src="{{  asset('img/dl/X-Out-Slide-3.jpg') }}">
+                <class="flex h-96 w-full bg-red-500 bg-center bg-cover img src="{{  asset('img/dl/X-Out-Slide-3.jpg') }}">
             </div>
         </div>
 
