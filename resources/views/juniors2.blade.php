bc0a4cb83a5511a49e26c19ac1db70ddde6ee211
Thomas <thomaswittke@web.de>
2025-11-23 12:15:42 +0100

    Update juniors2.blade.php

--- resources/views/juniors2.blade.php
+++ resources/views/juniors2.blade.php
@@ -82,7 +82,6 @@
                         <br>
                         Shirin
                         <br>
-                        <br>
                         
                     </div>
                 </div>
