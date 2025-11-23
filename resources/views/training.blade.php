657d75596429bf0f6a648b62ab129727a943ec62
Thomas <thomaswittke@web.de>
2025-11-23 12:11:44 +0100

    Update training.blade.php

--- resources/views/training.blade.php
+++ resources/views/training.blade.php
@@ -15,12 +15,12 @@
                     </div>
                     <h3>Trainingszeiten</h3>
                     <div>
-                        <div>Mittwoch: 17.30 Uhr – 19.30 Uhr</div>
+                        <div>Mittwoch: 17.30 – 19.30 Uhr</div>
                         <div>Gemeindehalle Feldkirchen</div>
                         <div>Richthofenstrasse 1</div>
                         <div>85622 Feldkirchen</div>
                         <br/>
-                        <div>Sonntag: 17.00 Uhr – 19.00 Uhr</div>
+                        <div>Sonntag: 17.00 – 19.00 Uhr</div>
                         <div>Dreifachturnhalle Feldkirchen</div>
                         <div>Olympiastr. 3</div>
                         <div>85622 Feldkirchen</div>
@@ -30,17 +30,17 @@
                     <h2>JUNIORS</h2>
                     <h3>Trainingszeiten</h3>
                     <div>
-                        <div>Montag: 18.00 Uhr – 20.00 Uhr</div>
+                        <div>Montag: 18.00 – 20.00 Uhr</div>
                         <div>Dreifachturnhalle Feldkirchen</div>
                         <div>Olympiastr. 3</div>
                         <div>85622 Feldkirchen</div>
                         <br/>
-                        <div>Mittwoch: 17.30 Uhr – 19.30 Uhr</div>
+                        <div>Mittwoch: 17.30 – 19.30 Uhr</div>
                         <div>Gemeindehalle Feldkirchen</div>
                         <div>Richthofenstrasse 1</div>
                         <div>85622 Feldkirchen</div>
                         <br/>
-                        <div>Sonntag: 17.00 Uhr – 19.00 Uhr</div>
+                        <div>Sonntag: 17.00 – 19.00 Uhr</div>
                         <div>Gemeindehalle Feldkirchen</div>
                         <div>Richthofenstrasse 1</div>
                         <div>85622 Feldkirchen</div>
@@ -50,17 +50,17 @@
                     <h2>SENIORS</h2>
                     <h3>Trainingszeiten</h3>
                     <div>
-                        <div>Montag: 20.00 Uhr – 22.00 Uhr</div>
+                        <div>Montag: 20.00 – 22.00 Uhr</div>
                         <div>Dreifachturnhalle Feldkirchen</div>
                         <div>Olympiastr. 3</div>
                         <div>85622 Feldkirchen</div>
                         <br>
-                        <div>Mittwoch: 19.30 Uhr – 21.30 Uhr</div>
+                        <div>Mittwoch: 19.30 – 21.30 Uhr</div>
                         <div>Gemeindehalle Feldkirchen</div>
                         <div>Richthofenstrasse 1</div>
                         <div>85622 Feldkirchen</div>
                         <br>
-                        <div>Sonntag: 19.00 Uhr – 21.00 Uhr</div>
+                        <div>Sonntag: 19.00 – 21.00 Uhr</div>
                         <div>Gemeindehalle Feldkirchen</div>
                         <div>Richthofenstrasse 1</div>
                         <div>85622 Feldkirchen</div>
